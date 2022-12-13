4869b9c3490d3252fddb98be55b2eab6e1cc5b93
Raghvendra Dixit <raghvendra_dixit@condenast.com>
2022-12-13 18:13:53 +0530

    adding shoppy gtm container

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import React, {useEffect} from 'react';
+import TagManager from 'react-gtm-module';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+useEffect(() => {
+  TagManager.initialize({ 
+    gtmId: 'GTM-XXXXX',//something like GTM-WN5KJ5KK
+ });
+}, []);
+return <Component {...pageProps} />
 }
 
 export default MyApp
